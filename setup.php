9b4093210d6a39cd102a9b5d3a02af72e17c979f
Sam Redmond <samredmondtech@gmail.com>
2019-11-10 17:40:56 +1300

    Indentation fix

--- setup.php
+++ setup.php
@@ -5,6 +5,7 @@
 	// Check if table exist
 	$query = "SELECT id FROM `" . $tablename . "`";
 	$result = mysqli_query($conn, $query);
+
 	if(empty($result)) {
 		// Create table
 		$sql = "CREATE TABLE " . $tablename . " (id INT(11) AUTO_INCREMENT PRIMARY KEY,recorded_time INT(11) UNIQUE,temp INT(11),rain INT(11),pop INT(11))";
